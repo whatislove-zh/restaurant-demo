9da93c2512ee29f010e3bbfbe8016609a73b8bd6
Vladislav <vladgm233223@gmail.com>
2023-07-04 13:43:48 +0300

    add loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { Slider } from "../components/Slider";
 import { selectBestFoodInfo } from "../store/features/getFoods/getFoodsSlice";
+import Loading from "../components/Loading";
 
 export const Home = () => {
   const { /*qty,*/ status, error } = useSelector(selectBestFoodInfo);
@@ -13,13 +14,7 @@ export const Home = () => {
       {status === "rejected" && <Typography>{error.message}</Typography>}
       {status === "loading" && (
         <>
-          <Typography width="320px" align="center" margin="auto">
-            The first download may take longer than usual, due to the fact that
-            the database is on a free server that shuts down if there are no
-            requests for a long time and it needs time to start
-          </Typography>
-
-          <Typography>Loading...</Typography>
+          <Loading />
         </>
       )}
       {status === "received" && (
